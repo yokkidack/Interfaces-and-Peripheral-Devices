27719109af13ee92d90dd8434c973a9b476596be
Zakhar Urbanovich <urbanovich.zahar@gmail.com>
2017-09-14 01:09:01 +0300

    Update source.cpp
    Using list to store transitional results

--- PCI/source.cpp
+++ PCI/source.cpp
@@ -3,8 +3,77 @@
  SetupAPI. Using external source of VEN&DEV
  for correlation.
 */
+#pragma comment (lib, "Setupapi.lib")
+#include <stdio.h>
+#include <windows.h>
+#include <setupapi.h>
+#include <devguid.h>
+#include <regstr.h>
+#include <locale.h>
+#include <list>
+#include <iterator>
+#include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, char* argv[]) {
- 
- return 0;
+char* getDeviceInfo(string);
+
+int main(int argc, char *argv[])
+{
+	HDEVINFO hDevInfo;
+	SP_DEVINFO_DATA deviceInfoData;
+	deviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
+	list<string> devices;
+
+	if ((hDevInfo = SetupDiGetClassDevs(NULL,
+		REGSTR_KEY_PCIENUM,
+		0,
+		DIGCF_PRESENT | DIGCF_ALLCLASSES)) == INVALID_HANDLE_VALUE)
+	exit(1);
+
+	for (DWORD i = 0; (SetupDiEnumDeviceInfo(hDevInfo, i, &deviceInfoData)); i++)
+	{
+		DWORD data;
+		LPTSTR buffer = NULL;
+		DWORD bufferSize = 0;
+
+		while (!SetupDiGetDeviceRegistryProperty(
+			hDevInfo,
+			&deviceInfoData,
+			SPDRP_HARDWAREID,
+			&data,
+			(PBYTE)buffer,
+			bufferSize,
+			&bufferSize))
+		{
+			if (GetLastError() == ERROR_INSUFFICIENT_BUFFER)//The data area passed to a system call is too small
+			{
+				if (buffer)
+					LocalFree(buffer);
+				buffer = (LPTSTR)LocalAlloc(LPTR, bufferSize * 2);
+			}
+			else
+				break;
+		}
+		
+		printf("%d. %s\n", i + 1, buffer);
+		devices.push_back((string)buffer);
+
+		if (buffer)
+			LocalFree(buffer);
+	}
+
+
+	getDeviceInfo(devices.front());
+	SetupDiDestroyDeviceInfoList(hDevInfo);
+
+	return 0;
+}
+
+char* getDeviceInfo(string str)
+{
+	string vendor = str.substr(4, 8);
+	string device = str.substr(13, 8);
+	cout << vendor << " " << device << endl;
+	return NULL;
 }
