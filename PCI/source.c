9d0e1a03985b2a5b8d1da4f1fb620aaf160fe941
Zakhar Urbanovich <urbanovich.zahar@gmail.com>
2017-09-13 10:12:19 +0300

    Update source.c

--- PCI/source.c
+++ PCI/source.c
@@ -1,1 +1,12 @@
+/*
+ PCI devices search. Using kernel API for communicating
+ with PCI bus.
+*/
 
+#include <linux/pci>
+
+int main(int argc, char* argv[]) 
+{
+  struct pci_dev* pci;
+  return 0; 
+}
