fde3724236d9366bf7279bfd362b91e16bed76b0
Zakhar Urbanovich <urbanovich.zahar@gmail.com>
2017-09-15 03:21:25 +0300

    Update source.cpp
    Final version

--- PCI/source.cpp
+++ PCI/source.cpp
@@ -1,8 +1,3 @@
-/*
- PCI devices identification. Using C++ and
- SetupAPI. Using external source of VEN&DEV
- for correlation.
-*/
 #pragma comment (lib, "Setupapi.lib")
 #include <stdio.h>
 #include <windows.h>
@@ -16,18 +11,19 @@
 #include <string>
 #include <fstream>
 #include <limits>
-//#include "header.h"
 #include "pci0.h"
 using namespace std;
 
-_PCI_DEVTABLE temp;
-_PCI_DEVTABLE search(_PCI_DEVTABLE&);
+list<_PCI_DEVTABLE> search(list<_PCI_DEVTABLE>&);
+list<_PCI_DEVTABLE> getDeviceInfo(list<string>);
+void print(list<_PCI_DEVTABLE>&);
 
-_PCI_DEVTABLE getDeviceInfo(string);
 string getInfo(char*, string);
 
 int main(int argc, char *argv[])
 {
+	list<_PCI_DEVTABLE> result;
+	list<_PCI_DEVTABLE> rt;
 	setlocale(LC_ALL, "RUSSIAN");
 	HDEVINFO hDevInfo;
 	SP_DEVINFO_DATA deviceInfoData;
@@ -50,14 +46,12 @@ int main(int argc, char *argv[])
 			hDevInfo,
 			&deviceInfoData,
 			SPDRP_HARDWAREID,
-			//SPDRP_DEVICEDESC,
 			&data,
-			//(PBYTE)buffer,
 			(PBYTE)buffer,
 			bufferSize,
 			&bufferSize))
 		{
-			if (GetLastError() == ERROR_INSUFFICIENT_BUFFER)//The data area passed to a system call is too small
+			if (GetLastError() == ERROR_INSUFFICIENT_BUFFER)
 			{
 				if (buffer)
 					LocalFree(buffer);
@@ -66,66 +60,66 @@ int main(int argc, char *argv[])
 			else
 				break;
 		}
-		
-		printf("%d. %s\n", i + 1, buffer);
+
 		devices.push_back((string)buffer);
 
 		if (buffer)
 			LocalFree(buffer);
 	}
 
+	list<_PCI_DEVTABLE> q = getDeviceInfo(devices);
+	result = search(q);
 
-	while (devices.size() != 0)
-	{
-		_PCI_DEVTABLE q = getDeviceInfo(devices.front());
-		search(q);
-		devices.pop_front();		
-	}
-	
-	//readCSV(argv[0], a);
+	print(result);
 	SetupDiDestroyDeviceInfoList(hDevInfo);
 
 	return 0;
 }
 
-_PCI_DEVTABLE getDeviceInfo(string str)
+void print(list<_PCI_DEVTABLE> &temp)
 {
-	std::string a = str;
-	string vendor = str.substr(8, 4);
-	//size_t pos = str.find("&SUBSYS");
-	//size_t pos1 = str.find("&DEV_");
-	string device = str.substr(17, 4);
-	//string device = str.substr(pos, pos1);
-	//string result = "\"" + vendor + "\"" + "," + "\"" + device + "\"";
-	string result0 = "0x" + vendor;
-	string result1 = "0x" + device;
-	cout << result0 + " " + result1 << endl;
-	return temp = { result0, result1, NULL, NULL };
-	//search(temp);
-	//cout << vendor + " " + device << endl;
-	//return vendor;
-	//return NULL;
-}
-
-string getInfo(char* path, string s)
-{
-	return NULL;
-}
-
-_PCI_DEVTABLE search(_PCI_DEVTABLE &temp)
-{
-	for (int i = 0; i < PCI_DEVTABLE_LEN; i++)
+	int i = 0;
+	while (temp.size() != 0)
 	{
-		if (temp.VenId == PciDevTable[i].VenId)
+		cout << i + 1 << ".VendorID: " << temp.front().VenId << " DeviceID: " << temp.front().DevId << endl;
+		cout << " Chip : " << temp.front().Chip << " Description: " << temp.front().ChipDesc << endl;
+		temp.pop_front();
+		i++;
+	}
+}
+
+list<_PCI_DEVTABLE> getDeviceInfo(list<string> str)
+{
+	list<_PCI_DEVTABLE> ty;
+	while (str.size() != 0)
+	{
+		_PCI_DEVTABLE temp;
+		string vendor = str.front().substr(8, 4);
+		string device = str.front().substr(17, 4);
+		string result0 = "0x" + vendor;
+		string result1 = "0x" + device;
+		temp = { result0, result1, NULL, NULL };
+		ty.push_front(temp);
+		str.pop_front();
+	}
+	return ty;
+}
+
+list<_PCI_DEVTABLE> search(list<_PCI_DEVTABLE>& temp)
+{
+	list<_PCI_DEVTABLE> devs;
+	while (temp.size() != 0)
+	{
+		for (int i = 0; i < PCI_DEVTABLE_LEN; i++)
 		{
-			if (temp.DevId == PciDevTable[i].DevId)
+			if (temp.front().VenId == PciDevTable[i].VenId && temp.front().DevId == PciDevTable[i].DevId)
 			{
-				//cout << "YEA!" << endl;
-				cout << PciDevTable[i].ChipDesc << endl;
-				return PciDevTable[i];
+				devs.push_front(PciDevTable[i]);
+				break;
 			}
 		}
+		temp.pop_front();
+		
 	}
-
-	return temp;
+	return devs;
 }
