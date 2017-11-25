cacad54ee5572ad5ae6bf0815aba871f53fe6c9c
Zakhar Urbanovich <urbanovich.zahar@gmail.com>
2017-11-26 02:12:12 +0300

    Repository initialized

---
+++ 4.USB/Laba_4/Manager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using MediaDevices;
+
+namespace Laba_4
+{
+    class Manager
+    {
+        public List<Usb> DeviseListCreate()
+        {
+            List<Usb> usbDevices = new List<Usb>();//список , который будет выводиться
+            List<DriveInfo> diskDrives = DriveInfo.GetDrives().Where(d => d.IsReady && d.DriveType == DriveType.Removable).ToList();//из драйверинфо получаем устройства, которые подключены и являются внешними
+            List<MediaDevice> mtpDrives = MediaDevice.GetDevices().ToList();//телефоны и тд
+            foreach (var device in mtpDrives)//медиа девайсы
+            {
+                device.Connect();
+                if (device.Protocol.Contains("MTP"))//поиск mtp девайсов
+                {
+                    usbDevices.Add(new Usb()
+                    {
+                        DeviceName = device.FriendlyName,
+                        FreeSpace = null,
+                        UsedSpace = null,
+                        TotalSpace = null,
+                        IsMtpDevice = true
+                    });
+                }
+            }
+            foreach (DriveInfo drive in diskDrives)//для флешек
+            {
+                usbDevices.Add(new Usb()
+                {
+                    DeviceName = drive.Name,
+                    FreeSpace = Convert(drive.TotalFreeSpace),
+                    UsedSpace = Convert(drive.TotalSize - drive.TotalFreeSpace),
+                    TotalSpace = Convert(drive.TotalSize),
+                    IsMtpDevice = false
+                });
+            }
+            return usbDevices;
+        }
+
+        private string Convert(long value)
+        {
+            double megaBytes = (value / 1024) / 1024;//перевод из байтов в в мегабайты
+            return megaBytes.ToString() + " mb";
+        }
+    }
+}
---
+++ 4.USB/Laba_4/Usb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsbEject;
+
+namespace Laba_4
+{
+    class Usb
+    {
+        public string DeviceName { get; set; }
+        public string FreeSpace { get; set; }
+        public string UsedSpace { get; set; }
+        public string TotalSpace { get; set; }
+        public bool IsMtpDevice { get; set; }
+      
+        public bool EjectDevice()
+        {
+            var tempName = this.DeviceName.Remove(2);//получаем том девавйса
+            var ejectedDevice = new VolumeDeviceClass().SingleOrDefault(v => v.LogicalDrive == tempName);//получаем значение состояния девайса
+            if (!IsMtpDevice)
+            {
+                ejectedDevice.Eject(false);
+                ejectedDevice = new VolumeDeviceClass().SingleOrDefault(v => v.LogicalDrive == tempName);//повторное подключение к девайсу после поытки извлечения
+            }
+            else
+            {
+                return false;
+            }
+            return ejectedDevice == null;
+        }
+    }
+}
