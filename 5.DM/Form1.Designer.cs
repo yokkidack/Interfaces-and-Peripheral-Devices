e614dc3434693f9bfd9f74668851725b505ad1d3
Zakhar Urbanovich <urbanovich.zahar@gmail.com>
2017-12-09 03:08:13 +0300

    Initial release

---
+++ 5.DM/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListOfDevices = new System.Windows.Forms.ListView();
+            this.DeviceDescription = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TurnOn = new System.Windows.Forms.Button();
+            this.TurnOff = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListOfDevices
+            // 
+            this.ListOfDevices.Location = new System.Drawing.Point(12, 32);
+            this.ListOfDevices.Name = "ListOfDevices";
+            this.ListOfDevices.Size = new System.Drawing.Size(196, 323);
+            this.ListOfDevices.TabIndex = 0;
+            this.ListOfDevices.UseCompatibleStateImageBehavior = false;
+            this.ListOfDevices.View = System.Windows.Forms.View.Tile;
+            // 
+            // DeviceDescription
+            // 
+            this.DeviceDescription.Location = new System.Drawing.Point(214, 32);
+            this.DeviceDescription.Multiline = true;
+            this.DeviceDescription.Name = "DeviceDescription";
+            this.DeviceDescription.Size = new System.Drawing.Size(355, 355);
+            this.DeviceDescription.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "List of devices";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Device description";
+            // 
+            // TurnOn
+            // 
+            this.TurnOn.Location = new System.Drawing.Point(12, 362);
+            this.TurnOn.Name = "TurnOn";
+            this.TurnOn.Size = new System.Drawing.Size(75, 23);
+            this.TurnOn.TabIndex = 4;
+            this.TurnOn.Text = "Turn on";
+            this.TurnOn.UseVisualStyleBackColor = true;
+            // 
+            // TurnOff
+            // 
+            this.TurnOff.Location = new System.Drawing.Point(133, 362);
+            this.TurnOff.Name = "TurnOff";
+            this.TurnOff.Size = new System.Drawing.Size(75, 23);
+            this.TurnOff.TabIndex = 5;
+            this.TurnOff.Text = "Turn off";
+            this.TurnOff.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 399);
+            this.Controls.Add(this.TurnOff);
+            this.Controls.Add(this.TurnOn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DeviceDescription);
+            this.Controls.Add(this.ListOfDevices);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Device Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView ListOfDevices;
+        private System.Windows.Forms.TextBox DeviceDescription;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button TurnOn;
+        private System.Windows.Forms.Button TurnOff;
+    }
+}
+
---
+++ 5.DM/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        private readonly List<Device> _devices;
+
+        private int _item;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _devices = DeviceManager.GetDevices();
+            foreach (var device in _devices)
+            {
+                ListOfDevices.Items.Add(device.Name);
+            }
+            TurnOn.Click += TurnOnClicked;
+            TurnOff.Click += TurnOffClicked;
+            ListOfDevices.Click += ItemSelected;
+        }
+
+        private void ShowInformation(Device device)
+        {
+            DeviceDescription.Text = "GUID: " + device.ClassGuid +
+                                     "\r\n\r\nHardwareID: " + device.HardwareId +
+                                     "\r\n\r\nManufacturer: " + device.Manufacturer +
+                                     "\r\n\r\nProvider: " + device.Provider +
+                                     "\r\n\r\nDriver description: " + device.Description +
+                                     "\r\n\r\nDevice path: " + device.DevicePath +
+                                     "\r\n\r\nSystem path: " + device.SysPath +
+                                     "\r\n\r\nStatus: " + device.Status;
+            TurnOn.Enabled = !device.Status;
+            TurnOff.Enabled = device.Status;
+        }
+
+        private void ItemSelected(object sender, EventArgs e)
+        {
+            ShowInformation(_devices[ListOfDevices.SelectedItems[0].Index]);
+            _item = ListOfDevices.SelectedItems[0].Index;
+        }
+
+        private void TurnOnClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                _devices[_item].ChangeConnection("Enable");
+                _devices[_item].Status = !_devices[_item].Status;
+                ShowInformation(_devices[ListOfDevices.SelectedItems[0].Index]);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Устройство не может быть включено.");
+            }
+        }
+
+        private void TurnOffClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                _devices[_item].ChangeConnection("Disable");
+                _devices[_item].Status = !_devices[_item].Status;
+                ShowInformation(_devices[ListOfDevices.SelectedItems[0].Index]);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Устройство не может быть отключено.");
+            }
+        }
+    }
+}
